38ef241b2222395f970dc9e893b92f5be8f964b0
GuanYQ0926 <guanyuqing0926@icloud.com>
2017-09-13 02:39:52 +0900

    ...

---
+++ 70_ClimbingStairs.py
@@ -0,0 +1,33 @@
+class Solution(object):
+
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # if n < 0:
+        #     return 0
+        # elif n == 0:
+        #     return 1
+        # else:
+        #     return self.climbStairs(n - 2) + self.climbStairs(n - 1)
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            prev_count = 2
+            pprev_count = 1
+            for i in xrange(3, n + 1):
+                temp = prev_count + pprev_count
+                pprev_count = prev_count
+                prev_count = temp
+                print temp
+            return prev_count
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.climbStairs(4))
