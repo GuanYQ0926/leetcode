3a4b9c528f257290bcae8db38ae6b81fbd54146b
GuanYQ0926 <guanyuqing0926@icloud.com>
2017-09-20 20:28:36 +0900

    ...

---
+++ 101_SymmetricTree.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+
+    def eachNode(self, left, right):
+        if left is None and right is None:
+            return True
+        if left is None or right is None:
+            return False
+        if left.val == right.val:
+            outter = self.eachNode(left.left, right.right)
+            inner = self.eachNode(left.right, right.left)
+            return outter and inner
+        else:
+            return False
+
+    def isSymmetric(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if not root:
+            return True
+        else:
+            return self.eachNode(root.left, root.right)
