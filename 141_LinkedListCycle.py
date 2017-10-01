b02bfa07acb7c703dc911cb6c0ca72da531d8785
GuanYQ0926 <guanyuqing0926@icloud.com>
2017-10-01 18:16:34 +0900

    ...

---
+++ 141_LinkedListCycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+class ListNode(object):
+
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        current = head
+        lead = head
+        while current and lead:
+            current = current.next
+            lead = lead.next.next
+            if current == lead:
+                return True
+        return False
