79f2c004b24a8443e1e5da4291813d3489c36c33
GuanYQ0926 <guanyuqing0926@icloud.com>
2017-10-06 14:30:36 +0900

    ...

---
+++ 172_FactorialTrailingZeroes.py
@@ -0,0 +1,12 @@
+class Solution(object):
+
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n > 0:
+            n /= 5
+            res += 5
+        return res
